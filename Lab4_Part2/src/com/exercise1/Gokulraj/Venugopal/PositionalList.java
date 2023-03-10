1988e0c268eb78a201d1844ac9159d4f386b6e57
Gokulraj Venugopal <gokulraj.venugopal010@gmail.com>
2023-03-10 18:43:46 -0500

    Assignment 4 Question 1_Part2

---
+++ Lab4_Part2/src/com/exercise1/Gokulraj/Venugopal/PositionalList.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2014, Michael T. Goodrich, Roberto Tamassia, Michael H. Goldwasser
+ *
+ * Developed for use with the book:
+ *
+ *    Data Structures and Algorithms in Java, Sixth Edition
+ *    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+ *    John Wiley & Sons, 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.exercise1.Gokulraj.Venugopal;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * An interface for positional lists.
+ * @author Michael T. Goodrich
+ * @author Roberto Tamassia
+ * @author Michael H. Goldwasser
+ * @see Position
+ */
+public interface PositionalList<E> extends Iterable<E> {
+
+  /**
+   * Returns the number of elements in the list.
+   * @return number of elements in the list
+   */
+  int size();
+
+  /**
+   * Tests whether the list is empty.
+   * @return true if the list is empty, false otherwise
+   */
+  boolean isEmpty();
+
+  /**
+   * Returns the first Position in the list.
+   *
+   * @return the first Position in the list (or null, if empty)
+   */
+  Position<E> first();
+
+  /**
+   * Returns the last Position in the list.
+   *
+   * @return the last Position in the list (or null, if empty)
+   */
+  Position<E> last();
+
+  /**
+   * Returns the Position immediately before Position p.
+   * @param p   a Position of the list
+   * @return the Position of the preceding element (or null, if p is first)
+   * @throws IllegalArgumentException if p is not a valid position for this list
+   */
+  Position<E> before(Position<E> p) throws IllegalArgumentException;
+
+  /**
+   * Returns the Position immediately after Position p.
+   * @param p   a Position of the list
+   * @return the Position of the following element (or null, if p is last)
+   * @throws IllegalArgumentException if p is not a valid position for this list
+   */
+  Position<E> after(Position<E> p) throws IllegalArgumentException;
+
+  /**
+   * Inserts an element at the front of the list.
+   *
+   * @param e the new element
+   * @return the Position representing the location of the new element
+   */
+  Position<E> addFirst(E e);
+
+  /**
+   * Inserts an element at the back of the list.
+   *
+   * @param e the new element
+   * @return the Position representing the location of the new element
+   */
+  Position<E> addLast(E e);
+
+  /**
+   * Inserts an element immediately before the given Position.
+   *
+   * @param p the Position before which the insertion takes place
+   * @param e the new element
+   * @return the Position representing the location of the new element
+   * @throws IllegalArgumentException if p is not a valid position for this list
+   */
+  Position<E> addBefore(Position<E> p, E e)
+    throws IllegalArgumentException;
+
+  /**
+   * Inserts an element immediately after the given Position.
+   *
+   * @param p the Position after which the insertion takes place
+   * @param e the new element
+   * @return the Position representing the location of the new element
+   * @throws IllegalArgumentException if p is not a valid position for this list
+   */
+  Position<E> addAfter(Position<E> p, E e)
+    throws IllegalArgumentException;
+
+  /**
+   * Replaces the element stored at the given Position and returns the replaced element.
+   *
+   * @param p the Position of the element to be replaced
+   * @param e the new element
+   * @return the replaced element
+   * @throws IllegalArgumentException if p is not a valid position for this list
+   */
+  E set(Position<E> p, E e) throws IllegalArgumentException;
+
+  /**
+   * Removes the element stored at the given Position and returns it.
+   * The given position is invalidated as a result.
+   *
+   * @param p the Position of the element to be removed
+   * @return the removed element
+   * @throws IllegalArgumentException if p is not a valid position for this list
+   */
+  E remove(Position<E> p) throws IllegalArgumentException;
+
+  /**
+   * Returns an iterator of the elements stored in the list.
+   * @return iterator of the list's elements
+   */
+  Iterator<E> iterator();
+
+  /**
+   * Returns the positions of the list in iterable form from first to last.
+   * @return iterable collection of the list's positions
+   */
+  Iterable<Position<E>> positions();
+  
+  /**
+   * Returns the current index of the element stored at the given Position.
+   * 
+   * @param p the Position of the element to be searched
+   * @return the index of the element stored at the given Position, or -1 if not found
+   */
+  public default Position<E> findPosition(E e) 
+  {
+      for (Position<E> p : positions()) 
+      {
+          if (e.equals(p.getElement())) 
+          {   
+              return p;
+          }
+      }
+      return null;
+  }
+  
+
+	/** 
+	 *************************************************
+	 *************************************************
+	 ********Student Name: Gokulraj Venugopal*********
+	 ********Student ID: 301202722********************
+	 *************************************************
+	 *************************************************
+	 **/
+
+  public static void main(String[] args) 
+  {
+	  PositionalList<String> list = new LinkedPositionalList<>();
+	  
+	  Position<String> a = list.addFirst("RED");
+	  Position<String> b = list.addLast("BLUE");
+	  Position<String> c = list.addAfter(b, "GREEN");
+	  Position<String> d = list.addAfter(c, "BLACK");
+	  Position<String> e = list.addLast("BLUE");
+	  
+	  Map<Position<String>, Character> positionMap = new HashMap<>();
+	  char letter = 'a';
+	  for (Position<String> p : list.positions()) {
+	      positionMap.put(p, letter);
+	      letter++;
+	  }
+	  
+	  System.out.println(list);
+	  
+	  System.out.println("Position of RED : " + positionMap.get(list.findPosition("RED"))); 
+	  System.out.println("Position of BLUE :  " + positionMap.get(list.findPosition("BLUE"))); 
+	  System.out.println("Position of BLACK :  " +positionMap.get(list.findPosition("BLACK")));
+	  System.out.println("Position of YELLOW :  " + positionMap.get(list.findPosition("YELLOW"))); 
+
+}
+}
+
